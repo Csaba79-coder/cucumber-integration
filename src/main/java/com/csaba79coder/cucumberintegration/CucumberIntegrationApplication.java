d6dd0d20fcd70c61bfa15f0074f19b8411634f88
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-15 18:33:33 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/cucumberintegration/CucumberIntegrationApplication.java
@@ -0,0 +1,17 @@
+package com.csaba79coder.cucumberintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberIntegrationApplication {
+
+    // https://www.youtube.com/watch?v=fl9pv_lZVmA
+    // https://github.com/ekim197711/springboot-cucumber/blob/main/pom.xml
+    // https://www.youtube.com/watch?v=TBxb5mx2MvI
+    // https://github.com/ekim197711
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/cucumberintegration/CucumberIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.cucumberintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
